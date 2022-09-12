bc60ea1c914dfc0dfa7bd41ca73b1b3ac978e468
jinittrivedi003 <112106250+jinittrivedi003@users.noreply.github.com>
2022-09-12 10:35:26 +0530

    New one
    Csv upload and download

--- theme/urls/category.py
+++ theme/urls/category.py
@@ -5,4 +5,7 @@ categoryURL=[
     path('addCategory/',addCategory,name="addCategory"),  
     path('updateCatrogry/<id>',updateCategory,name="updateCategory"), 
     path('deleteCategory/<id>',deleteCategory,name="deleteCategory"), 
+    path('bulkUpload/',bulk_upload,name='bulkUpload'),
+    path('upload_csv/',upload_csv,name='upload_csv'),
+    path('download_csv/',download_csv,name='download_csv'),
 ]
--- theme/views/category.py
+++ theme/views/category.py
@@ -1,9 +1,12 @@
+from http.client import HTTPResponse
 from django.shortcuts import get_object_or_404,render,redirect
 from ..models import CategoryModel
 from django.shortcuts import render
 #from ..models import Category as CategoryForm
 from ..forms.category_form import CategoryForm
 #from ..models import Category
+from django.http import HttpResponse
+import csv
 
 def viewCategory(request):
     context={}
@@ -36,3 +39,40 @@ def deleteCategory(request,id):
         obj.delete()
         return redirect("viewCategory")
     return render(request,"category/view.html",context)
+
+def bulk_upload(request):
+    return render(request,"category/bulkUpload.html")
+
+def upload_csv(request):
+    if("GET" == request.method):
+        return HTTPResponse("Not Valid method")
+    csv_file=request.FILES["csv_file"]
+    if not csv_file.name.endswith('.csv'):
+        return HTTPResponse("File not valid")
+    if csv_file.multiple_chunks():
+        return HTTPResponse("Upload file is big")
+    file_data = csv_file.read().decode("utf-8")
+    lines = file_data.split("\n")
+    c=len(lines)
+    # return HttpResponse(line[0])
+    
+    for i in range(0,c-1):
+        fields = lines[i].split(",")
+        data_dict = {}
+        data_dict["title"] = fields[0]
+        data_dict["description"] = fields[1]
+
+        cform=CategoryForm(data_dict)
+        if cform.is_valid():
+            cform.save()
+    return redirect("viewCategory")
+
+def download_csv(request):
+    response = HttpResponse('text/csv')
+    response['Content-Disposition'] = 'attachment; filename=category.csv'
+    writer = csv.writer(response)
+    writer.writerow(['title','description'])
+    for data in CategoryModel.objects.all():
+        writer.writerow([data.title,data.description])
+
+    return response
